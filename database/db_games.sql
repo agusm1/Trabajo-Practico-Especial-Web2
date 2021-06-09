5342f816d0cfb924b7a7b382b8396daf9a727782
unknown <agustinmarcosf@gmail.com>
2021-06-09 00:52:47 -0300

    Actualizando repositorio v1.1

--- database/db_games.sql
+++ database/db_games.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-05-2021 a las 05:11:47
+-- Tiempo de generación: 09-06-2021 a las 05:43:15
 -- Versión del servidor: 10.1.31-MariaDB
--- Versión de PHP: 7.2.4
+-- Versión de PHP: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,25 +29,35 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `game` (
-  `id_game` bigint(20) UNSIGNED NOT NULL,
-  `titulo` varchar(260) NOT NULL,
-  `anio` smallint(5) NOT NULL,
-  `sinopsis` text NOT NULL,
-  `id_genero` int(11) NOT NULL
+  `id_game` int(11) NOT NULL,
+  `title` varchar(260) NOT NULL,
+  `year` smallint(5) NOT NULL,
+  `synopsis` text NOT NULL,
+  `id_genre` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `game`
 --
 
-INSERT INTO `game` (`id_game`, `titulo`, `anio`, `sinopsis`, `id_genero`) VALUES
+INSERT INTO `game` (`id_game`, `title`, `year`, `synopsis`, `id_genre`) VALUES
 (1, 'The Legend of Zelda: Link\'s Awakening', 2019, 'Como Link, despertaste en una extraña tierra muy lejos de Hyrule, donde los animales hablan y los monstruos merodean por ahí. En esta nueva versión de uno de los juegos más queridos de The Legend of Zelda, que ha sido mejorada con increíbles detalles, deberás explorar la isla Koholint y todas sus mazmorras cargadas de trampas ocultas para descubrir la verdad detrás de tu paradero y despertar al Pez del Viento. Conforme avances, encontrarás una gran variedad de divertidísimos y encantadores personajes de los cuales no querrás despedirte.\r\n', 2),
 (2, 'The Last of Us', 2013, '¿Qué pasaría si la naturaleza comenzara a reclamar lo suyo? Esta es la pregunta que da el giro argumental principal de la historia en The Last of Us. Los científicos han descubierto un hongo muy peligroso que hace que a las personas les broten estructuras parecidas a hongos valga la redundancia y que su conducta sea salvaje y peligrosa, el virus ha comenzado a mutar a la humanidad hasta convertirlos en algo parecido a zombis.', 1),
 (3, 'The Binding of Isaac', 2011, 'El protagonista y su madre viven felizmente en una pequeña casa en una colina, con Isaac dibujando y jugando con sus juguetes, a la vez que su madre ve transmisiones cristianas en la televisión. La madre de Isaac escucha entonces “una voz superior” que afirma la corrupción de su hijo por el pecado, necesitando una salvación. ', 2),
 (5, 'Donkey Kong 2', 1990, 'una erupción volcánica devolvió la anteriormente hundida Crocodile Isle (Isla Cocodrilo) de vuelta a la superficie del océano, así K. Rool termina secuestrando una vez más a Donkey Kong.', 1),
 (6, 'Sacred', 2004, 'Ancaria está en una profunda crisis. Los humanos luchan contra las hordas de orcos que proceden del sur, éstos a su paso arrasan ciudades y poblados en los cuales masacran a sus habitantes. Los soldados del rey, al mando del Príncipe Vorian, intentan repeler la invasión orca. Pero estos orcos no están intentando conquistar nuevos territorios, sino que huyen de algo que no había ocurrido nunca en el mundo: los muertos se levantan de sus tumbas con miradas hostiles.', 3),
 (7, 'Uncharted: El tesoro de Drake', 2007, 'El juego se abre con Nathan Drake (Nolan North, voz inglesa) quien recupera el ataúd del explorador y ancestro de Nathan (tal como él mismo dice), Sir Francis Drake desde el fondo del océano, utilizando las coordenadas inscritas en un anillo en posesión de Nathan. El esfuerzo se paga a través de la empresa de la periodista Elena Fisher (Emily Rose), que está ahí para registrar los acontecimientos para un posible documental exitoso. El ataúd está vacío, sin cadáver, sólo contiene un diario escrito por Sir Francis Drake, que apunta a la ubicación de El Dorado, la legendaria ciudad de oro donde Sir Francis Drake fue para encontrar el tesoro, demostrando su falsa muerte como Nathan creía. ', 1),
-(8, 'The Elder Scrolls V: Skyrim', 2011, 'La agitación se extiende por el Imperio de Tamriel. El soberano de Skyrim ha sido asesinado. A medida que surgen aspirantes al trono, se van formando alianzas. En mitad de este conflicto, resurge un antiguo mal mucho más peligroso: los dragones, olvidados hace tiempo entre oscuros pasajes de los antiguos pergaminos, han regresado a Tamriel. El futuro de Skyrim, incluso el del propio Imperio, pende de un hilo, mientras aguardan la llegada vaticinada del Sangre de dragón, un héroe dotado del poder de La Voz, el único que puede hacer frente a los dragones.', 3);
+(8, 'The Elder Scrolls V: Skyrim', 2011, 'La agitación se extiende por el Imperio de Tamriel. El soberano de Skyrim ha sido asesinado. A medida que surgen aspirantes al trono, se van formando alianzas. En mitad de este conflicto, resurge un antiguo mal mucho más peligroso: los dragones, olvidados hace tiempo entre oscuros pasajes de los antiguos pergaminos, han regresado a Tamriel. El futuro de Skyrim, incluso el del propio Imperio, pende de un hilo, mientras aguardan la llegada vaticinada del Sangre de dragón, un héroe dotado del poder de La Voz, el único que puede hacer frente a los dragones.', 3),
+(14, 'Super Smash Bros: Brawl', 2008, 'Tras elegir entre una gran variedad de personajes, dos a cuatro jugadores lucharán en diferentes y variados escenarios mientras tratan de sacar a sus oponentes fuera del escenario. ... Incluye un modo de un jugador más extenso que sus predecesores, conocido como El emisario subespacial.', 1),
+(20, '1324', 3132, '133435', 1),
+(21, '1324', 3132, '133435', 1),
+(22, 'dgssfz', 0, 'gddj', 1),
+(23, 'dgssfz', 0, 'gddj', 1),
+(24, 'dgssfz', 0, 'gddj', 1),
+(25, 'dgssfz', 0, 'gddj', 1),
+(26, 'dgssfz', 0, 'gddj', 1),
+(27, 'dgssfz', 0, 'gddj', 1),
+(28, 'dgssfz', 0, 'gddj', 1);
 
 -- --------------------------------------------------------
 
@@ -56,19 +66,30 @@ INSERT INTO `game` (`id_game`, `titulo`, `anio`, `sinopsis`, `id_genero`) VALUES
 --
 
 CREATE TABLE `genero` (
-  `id_genero` int(11) NOT NULL,
-  `nombre` varchar(260) NOT NULL
+  `id_genre` int(11) NOT NULL,
+  `name` varchar(260) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `genero`
 --
 
-INSERT INTO `genero` (`id_genero`, `nombre`) VALUES
+INSERT INTO `genero` (`id_genre`, `name`) VALUES
 (1, 'Accion'),
 (2, 'Aventura'),
-(3, 'Rol'),
-(4, 'Carreras');
+(3, 'Rol');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `image`
+--
+
+CREATE TABLE `image` (
+  `id_image` int(11) NOT NULL,
+  `path` varchar(100) NOT NULL,
+  `id_game` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -99,13 +120,20 @@ INSERT INTO `user` (`id`, `usuario`, `contraseña`) VALUES
 ALTER TABLE `game`
   ADD PRIMARY KEY (`id_game`),
   ADD UNIQUE KEY `id_game` (`id_game`),
-  ADD KEY `fk_game_genero` (`id_genero`);
+  ADD KEY `fk_game_genero` (`id_genre`);
 
 --
 -- Indices de la tabla `genero`
 --
 ALTER TABLE `genero`
-  ADD PRIMARY KEY (`id_genero`);
+  ADD PRIMARY KEY (`id_genre`);
+
+--
+-- Indices de la tabla `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id_image`),
+  ADD KEY `fk_image_game` (`id_game`);
 
 --
 -- Indices de la tabla `user`
@@ -121,13 +149,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `game`
 --
 ALTER TABLE `game`
-  MODIFY `id_game` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_game` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de la tabla `genero`
 --
 ALTER TABLE `genero`
-  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_genre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `image`
+--
+ALTER TABLE `image`
+  MODIFY `id_image` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
@@ -143,7 +177,13 @@ ALTER TABLE `user`
 -- Filtros para la tabla `game`
 --
 ALTER TABLE `game`
-  ADD CONSTRAINT `fk_game_genero` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`id_genero`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_game_genero` FOREIGN KEY (`id_genre`) REFERENCES `genero` (`id_genre`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `image`
+--
+ALTER TABLE `image`
+  ADD CONSTRAINT `fk_image_game` FOREIGN KEY (`id_game`) REFERENCES `game` (`id_game`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
